6b2c95287c01f5d66da17ca10a5018f42a87cdf6
Gustavo Sizílio Nery <gustavosizilio@gmail.com>
2023-02-21 10:51:33 -0300

    cleanup

--- javascript/tradepanel/test/.d.ts
+++
@@ -1,8 +0,0 @@
-// In a `.d.ts` file within the included folders of your project
-import Moralis from 'Moralis';
-
-declare global {
-    export const Moralis : Moralis;
-    export const ExecuteFunctionCallResult : Moralis.ExecuteFunctionCallResult;
-}
-
