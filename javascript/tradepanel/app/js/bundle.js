3fb18f75c19203a297cefa3332099832f7b2cf7b
Gustavo Sizílio Nery <gustavosizilio@gmail.com>
2023-02-21 09:55:51 -0300

    fix mumbai network url

--- javascript/tradepanel/app/js/bundle.js
+++ javascript/tradepanel/app/js/bundle.js
@@ -25378,14 +25378,15 @@ class mumbaiNetwork extends TestNetwork {
         super();
         this.ChainId = 80001;
         this.Name = "mumbai";
-        this.ChainName = 'Polygon Mumbai';
+        this.ChainName = "Polygon Mumbai";
         this.NativeCurrencyName = "Matic";
         this.NativeSymbol = "MATIC";
         this.NativeDecimal = 18;
-        this.RpcUrl = 'https://rpc-mumbai.matic.today/';
-        this.BlockExplorer = 'https://mumbai.polygonscan.com/';
-        this.FaucetUrl = 'https://faucet.polygon.technology/';
-        this.BuyUrl = 'https://www.moonpay.com/buy/matic';
+        this.RpcUrl =
+            "https://polygon-mumbai.g.alchemy.com/v2/FILTBk8FT9RzcRVxkMXIg7Ipl8JCNdK2";
+        this.BlockExplorer = "https://mumbai.polygonscan.com/";
+        this.FaucetUrl = "https://faucet.polygon.technology/";
+        this.BuyUrl = "https://www.moonpay.com/buy/matic";
     }
 }
 
--- javascript/tradepanel/src/networks/mumbai-network.ts
+++ javascript/tradepanel/src/networks/mumbai-network.ts
@@ -1,22 +1,20 @@
-import Network from './Network';
+import Network from "./Network";
 import TestNetwork from "./TestNetwork";
 
 export default class mumbaiNetwork extends TestNetwork {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.ChainId = 80001;
-        this.Name = "mumbai";
-        this.ChainName = 'Polygon Mumbai';
-        this.NativeCurrencyName = "Matic";
-        this.NativeSymbol = "MATIC";
-        this.NativeDecimal = 18;
-        this.RpcUrl = 'https://rpc-mumbai.matic.today/';
-        this.BlockExplorer = 'https://mumbai.polygonscan.com/';
-        this.FaucetUrl = 'https://faucet.polygon.technology/';
-        this.BuyUrl = 'https://www.moonpay.com/buy/matic';
-    }
-
-
+    this.ChainId = 80001;
+    this.Name = "mumbai";
+    this.ChainName = "Polygon Mumbai";
+    this.NativeCurrencyName = "Matic";
+    this.NativeSymbol = "MATIC";
+    this.NativeDecimal = 18;
+    this.RpcUrl =
+      "https://polygon-mumbai.g.alchemy.com/v2/FILTBk8FT9RzcRVxkMXIg7Ipl8JCNdK2";
+    this.BlockExplorer = "https://mumbai.polygonscan.com/";
+    this.FaucetUrl = "https://faucet.polygon.technology/";
+    this.BuyUrl = "https://www.moonpay.com/buy/matic";
+  }
 }
-
