6446fccb097959e33ec6392b1dfdd5f63f966a87
Gustavo Sizílio <gustavosizilio@gmail.com>
2023-03-28 10:08:41 -0300

    fix sh script

--- javascript/tradepanel/test/docker-run-test.sh
+++ javascript/tradepanel/test/docker-run-test.sh
@@ -2,7 +2,10 @@
 
 cd javascript/tradepanel
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install
 nvm use
 npm install
