4bdc77b54b4c27b8a4a0e76f7944f9e75e8b4c64
ingig <ingigauti@gmail.com>
2023-04-20 10:00:18 +0000

    fix import for KycStatus

--- javascript/tradepanel/app/types/services/blockchain/BlockchainService.d.ts
+++ javascript/tradepanel/app/types/services/blockchain/BlockchainService.d.ts
@@ -1,12 +1,10 @@
 import BaseService from "../backend/BaseService";
-import { BigNumber } from "ethers";
 import Network from "../../networks/Network";
 import ContractAddresses from "../../contracts/ContractAddresses";
 export default class BlockchainService extends BaseService {
     network: Network;
     contracts: ContractAddresses;
     constructor();
-    protected getBalanceOf(tokenAddress: any, ethAddress: string): Promise<BigNumber>;
     protected loadEther(): Promise<void>;
     getNativeBalance(): Promise<string>;
     balanceOfAbi: {
